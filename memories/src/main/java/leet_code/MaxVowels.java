90a2bcd6e22dd2e8ceb947c9d95c1766643a4a12
yashodhah <y.dominatz@gmail.com>
2023-05-21 13:19:57 +0530

    leet_code | vowels | less optimized - large input failing

--- memories/src/main/java/leet_code/MaxVowels.java
+++ memories/src/main/java/leet_code/MaxVowels.java
@@ -1,7 +1,5 @@
 package leet_code;
 
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Set;
 
 public class MaxVowels {
@@ -12,30 +10,26 @@ public class MaxVowels {
         int maxSoFar = 0;
 
         while (s.length() - start > k - 1) {
-            VowelInfo vowelInfo = getNuOfVowels(s, start, start + k);
+            VowelInfo vowelInfo = getNuOfVowels(s, start, k);
 
             if (vowelInfo.count() > maxSoFar) {
                 maxSoFar = vowelInfo.count();
             }
 
-            if (vowelInfo.count() == k) {
+            if (vowelInfo.count() == k || start == s.length() - k) {
                 break;
             }
 
-//            start = vowelInfo.startIdx();
-            start++;
+            start = Math.min(s.length() - k, vowelInfo.startIdx());
         }
 
         return maxSoFar;
     }
 
-    private VowelInfo getNuOfVowels(String s, int start, int end) {
+    private VowelInfo getNuOfVowels(String s, int start, int window) {
         int count = 0;
-//        int idxLastVowel = -1;
-
-//        if (end > s.length()) {
-//            start = start - (s.length() - end);
-//        }
+        int idxLastVowel = -1;
+        int end = start + window;
 
         String sub = s.substring(start, end);
 
@@ -45,25 +39,24 @@ public class MaxVowels {
             if (vowels.contains(c)) {
                 count++;
 
-//                if (idxLastVowel < 0) {
-//                    idxLastVowel = i;
-//                }
+                if (i != 0 && idxLastVowel < 0) {
+                    idxLastVowel = i;
+                }
             }
         }
 
-//        if (idxLastVowel < 0) {
-//            idxLastVowel = end;
-//        } else {
-//            idxLastVowel = start + idxLastVowel;
-//        }
+        if (idxLastVowel < 0) {
+            idxLastVowel = end;
+        } else {
+            idxLastVowel = start + idxLastVowel;
+        }
 
-        return new VowelInfo(count, -1);
+        return new VowelInfo(count, idxLastVowel);
     }
 
     public static void main(String[] args) {
         MaxVowels m = new MaxVowels();
         int count = m.maxVowels("abciiidef", 3);
-
         System.out.println(count);
     }
 }
